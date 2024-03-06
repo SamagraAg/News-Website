c6b8f19890a0d73c37087c6c912cf947540a1ea0
Samagra Agrawal <samagra110@gmail.com>
2024-03-06 20:45:05 +0530

    Added Prev next buttons to news component

--- src/Components/News.js
+++ src/Components/News.js
@@ -10,25 +10,33 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
+            page: 1
         }
     }
     async componentDidMount() {
         const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&pageSize=${pageSize}`
         const response = await fetch(url);
         const parsedResponse = await response.json()
-        this.setState({articles: parsedResponse.articles})
+        this.setState({ articles: parsedResponse.articles })
     }
     render() {
         return (
-            <div className='container my-2'>
-                <h1>Top Headlines</h1>
-                <div className="row">
-                    {this.state.articles.map((article) => {
-                        return <div className="col-md-4">
-                            <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage} url={article.url}></NewsItem>
-                        </div>
-                    })}
+            <div>
+                <div className='container my-2'>
+                    <h1>Top Headlines</h1>
+                    <div className="row">
+                        {this.state.articles.map((article) => {
+                            return <div className="col-md-4">
+                                <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage} url={article.url}></NewsItem>
+                            </div>
+                        })}
 
+                    </div>
+                </div>
+                <div className="container mt-3 d-flex justify-content-between">
+                    <button type="button" className="btn btn-dark btn-sm">&laquo; Previous</button>
+                    <p>Page: {this.state.page}</p>
+                    <button type="button" className="btn btn-dark btn-sm">Next &raquo;</button>
                 </div>
             </div>
         )
