3facb3824165e91e3a8ed0f378983fd450f43faf
Samagra Agrawal <samagra110@gmail.com>
2024-03-07 18:29:01 +0530

    Add Loading Spinner

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Loading from './Loading'
 
 const apiKey = "d0081e97c02642b0a7efae8e5d07a213"
 const pageSize = 9
@@ -9,7 +10,7 @@ export class News extends Component {
         super()
         this.state = {
             articles: [],
-            loading: false,
+            loading: true,
             page: 1,
             totalResultsFetch: 0
         }
@@ -18,32 +19,32 @@ export class News extends Component {
         const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&pageSize=${pageSize}&page=${this.state.page}`
         const response = await fetch(url);
         const parsedResponse = await response.json()
-        this.setState({ articles: parsedResponse.articles })
-        this.setState({ totalResultsFetch: parsedResponse.totalResults })
+        this.setState({ articles: parsedResponse.articles, totalResultsFetch: parsedResponse.totalResults, loading: false })
     }
     handlePrevClick = async () => {
+        this.setState({ loading: true });
         console.log(`Prev Click`);
         const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&pageSize=${pageSize}&page=${this.state.page - 1}`
         const response = await fetch(url);
         const parsedResponse = await response.json()
-        this.setState({ articles: parsedResponse.articles })
-        this.setState({ page: this.state.page - 1 })
+        this.setState({ articles: parsedResponse.articles, page: this.state.page - 1, loading: false })
     }
     handleNextClick = async () => {
+        this.setState({ loading: true });
         console.log(`Next Click`);
         const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}&pageSize=${pageSize}&page=${this.state.page + 1}`
         const response = await fetch(url);
         const parsedResponse = await response.json()
-        this.setState({ articles: parsedResponse.articles })
-        this.setState({ page: this.state.page + 1 })
+        this.setState({ articles: parsedResponse.articles, page: this.state.page + 1, loading: false })
     }
     render() {
         return (
             <div>
                 <div className='container my-2'>
-                    <h1>Top Headlines</h1>
+                    <h1 className='text-center'>Top Headlines</h1>
+                    {this.state.loading && <Loading></Loading>}
                     <div className="row">
-                        {this.state.articles.map((article) => {
+                        {!this.state.loading && this.state.articles.map((article) => {
                             return <div className="col-md-4" key={article.url}>
                                 <NewsItem title={article.title} description={article.description} imageUrl={article.urlToImage} url={article.url}></NewsItem>
                             </div>
