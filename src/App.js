a32ee8684e716b8784212ce1c8b0b6c55eda4cf5
Samagra Agrawal <samagra110@gmail.com>
2024-03-08 20:26:27 +0530

    Setup React Router to fetch news category wise

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom"
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
-        <News pageSize="9" country="in" category="sports"></News>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<News key="general" pageSize="9" country="in" category="general"></News>} />
+            <Route path="/business" element={<News key="business" pageSize="9" country="in" category="business"></News>} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize="9" country="in" category="entertainment"></News>} />
+            <Route path="/health" element={<News key="health" pageSize="9" country="in" category="health"></News>} />
+            <Route path="/science" element={<News key="science" pageSize="9" country="in" category="science"></News>} />
+            <Route path="/sports" element={<News key="sports" pageSize="9" country="in" category="sports"></News>} />
+            <Route path="/technology" element={<News key="technology" pageSize="9" country="in" category="technology"></News>} />
+          </Routes>
+        </Router>
       </>
     )
   }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,58 +1,59 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#35374B" }} data-bs-theme="dark">
                 <div className="container-fluid">
                     <img src="favicon-32x32.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top " />
-                    <a className="mx-2 navbar-brand" href="/">KhabarDaar</a>
+                    <Link className="mx-2 navbar-brand" to="/">KhabarDaar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Business</a>
+                                <Link className="nav-link" to="/business">Business</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Entertainment</a>
+                                <Link className="nav-link" to="/entertainment">Entertainment</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Health</a>
+                                <Link className="nav-link" to="/health">Health</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Science</a>
+                                <Link className="nav-link" to="/science">Science</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Sports</a>
+                                <Link className="nav-link" to="/sports">Sports</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Technology</a>
+                                <Link className="nav-link" to="/technology">Technology</Link>
                             </li>
                             <li className="nav-item">
                             </li>
                             {/* <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/contact">Contact</a>
+                                <Link className="nav-link" to="/contact">Contact</Link>
                             </li> */}
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Country
-                                </a>
+                                </Link>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">India</a></li>
-                                    <li><a className="dropdown-item" href="/">USA</a></li>
-                                    <li><a className="dropdown-item" href="/">China</a></li>
-                                    <li><a className="dropdown-item" href="/">Russia</a></li>
+                                    <li><Link className="dropdown-item" to="/">India</Link></li>
+                                    <li><Link className="dropdown-item" to="/">USA</Link></li>
+                                    <li><Link className="dropdown-item" to="/">China</Link></li>
+                                    <li><Link className="dropdown-item" to="/">Russia</Link></li>
                                 </ul>
                             </li>
                             {/* <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <Link className="nav-link disabled" aria-disabled="true">Disabled</Link>
                             </li> */}
                         </ul>
                         <form className="d-flex" role="search">
